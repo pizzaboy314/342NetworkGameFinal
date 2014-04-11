05081de71402d7287083d5bb7a70374a1d45613a
sgm1 <sgmathai@att.net>
2014-04-10 21:30:58 -0500

    prompt box

--- src/gui/GuiLauncher.java
+++ src/gui/GuiLauncher.java
@@ -1,11 +1,22 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 public class GuiLauncher extends JFrame implements Runnable{
 
 	public GuiLauncher() {
+		int choice = JOptionPane.showConfirmDialog(this,
+				"Are you a server?", "Start Up",
+				JOptionPane.YES_NO_OPTION);
+		if (choice == 0){
+			System.out.println("first");
+			//TODO
+		}else{
+			System.out.println("sec");
+			//TODO
+		}
 		this.setSize(300, 300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
@@ -18,6 +29,7 @@ public class GuiLauncher extends JFrame implements Runnable{
 	@Override
 	public void run() {
 		setVisible(true);
+			
 	}
 
 }
